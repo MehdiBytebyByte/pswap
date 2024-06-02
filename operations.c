a0e64d4cbc215dc0d1c6c85868bbc5263068ef9f
Mehdi Boughrara <mboughra@e3r10p16.1337.ma>
2024-06-02 23:07:14 +0100

    commit

--- operations.c
+++ operations.c
@@ -6,7 +6,7 @@
 /*   By: mboughra <mboughra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/02 00:22:06 by mboughra          #+#    #+#             */
-/*   Updated: 2024/06/02 03:17:14 by mboughra         ###   ########.fr       */
+/*   Updated: 2024/06/02 03:26:16 by mboughra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,19 +26,38 @@
 
 t_stack *swap(t_stack **stack)
 {
-    t_stack *first;
-    t_stack *second;
-
-    if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
-        return *stack; // No swap needed if stack is empty or has only one element
-
-    first = *stack;
-    second = first->next;
-
-    // Performing the swap
-    first->next = second->next;
+	t_stack	*first;
+	t_stack	*second;
+	
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		return *stack;
+		
+	first = *stack;
+	second = first->next;
+	first->next = second->next;
     second->next = first;
     *stack = second;
-
-    return *stack; // Returning the new head of the stack
+	return (*stack);
+};
+t_stack *rotate(t_stack **stack)
+{
+	t_stack	*last;
+	t_stack	*first;
+	
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		return *stack;
+		
+	last = *stack;
+	first = *stack;
+	while (last)
+	{
+		if (last->next )
+		{
+			/* code */
+		}
+			
+	}
+	
+	
+	return (*stack);
 };
